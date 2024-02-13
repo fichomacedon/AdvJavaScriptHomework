81a761bbbb58fe9103eec5c033e187a5fb4d493b
Filip.kostadinovski <72503214+fichomacedon@users.noreply.github.com>
2024-02-13 19:59:57 +0100

    Add files via upload

---
+++ objects exercises/app.js
@@ -0,0 +1,11 @@
+console.log("hello")
+const movie = {
+    title: "Saving Private Ryan",
+    director: "Steven Spielberg",
+    country: "USA",
+    year: 1999,
+    productionInfo: {
+        budget: "70 million",
+        producer: "Ian Bryce",
+    }
+}
